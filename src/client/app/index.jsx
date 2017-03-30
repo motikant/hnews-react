8d042d8ce514cd78d6fdbf1e25201af938bf7813
motikant-sw <motikant.polai@snapwiz.com>
2017-03-31 00:59:15 +0530

    Pagination -- partially working

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -7,18 +7,39 @@ class App extends React.Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			posts :[]
+			postIds:[],
+			posts :[],
+			offset:0,
+			pageSize:10
 		};
-		
+		this.goNext = this.goNext.bind(this);	
+		this.goPrev = this.goPrev.bind(this);	
 	}
-	getNews(ids = []){
+	goNext(e){
 		let self = this;
-		let allNews = ids.slice(0,10).map((id)=>{
+		this.setState((prevState)=>{
+			self.getNews(prevState.offset +prevState.pageSize,prevState.pageSize);
+			return{
+				offset: Math.min(prevState.postIds.length , prevState.offset + prevState.pageSize)
+			};
+		});
+	}
+	goPrev(e){
+		let self = this;
+		this.setState((prevState)=>{
+			self.getNews(prevState.offset +prevState.pageSize,prevState.pageSize);
+			return{
+				offset: Math.max(0 , prevState.offset - prevState.pageSize)
+			};
+		});
+	}
+	getNews(start,size){
+		let self = this;
+		let allNews = this.state.postIds.slice(start,start+size).map((id)=>{
 			return new Grab({
 				url:"https://hacker-news.firebaseio.com/v0/item/"+id+".json"
 			});		
 		});
-			console.log(allNews);
 		Promise.all(allNews).then((results)=>{
 			results = results.map((a)=>JSON.parse(a));
 			self.setState({
@@ -32,14 +53,23 @@ class App extends React.Component {
 		});
 		let self = this;
 		latest.then((resp)=>{
-			self.getNews(JSON.parse(resp));
+			self.setState({
+				postIds: JSON.parse(resp)
+			});
+			self.getNews(this.state.offset , this.state.pageSize);
 		});
 	}
   render () {
       return (
 		  <div>
-		  <p> Hacker News React! </p>
-		  <NewsList posts={this.state.posts}/>
+			  <p> Hacker News React! </p>
+			  <div className="row">
+				<div className="col-12">
+					<button className="btn btn-primary m-2" onClick={this.goPrev}>Prev</button>
+					<button className="btn btn-primary m-2" onClick={this.goNext}>Next</button>
+				</div>
+			  </div>
+			  <NewsList posts={this.state.posts}/>
 		  </div>
 	  );
     }
